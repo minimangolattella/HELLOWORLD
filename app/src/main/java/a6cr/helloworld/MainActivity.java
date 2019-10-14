dd662c5933e0141b395b5b48ddef5676d1ce8e25
minimangolattella <lukas-meinhard.koller@brg-judenburg.ac.at>
2019-10-14 13:48:55 +0200

    added second comment

--- app/src/main/java/a6cr/helloworld/MainActivity.java
+++ app/src/main/java/a6cr/helloworld/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     // Meine erste App -  Das ist die Java-Datei zur Hauptoberfläche
+    // Hallo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
